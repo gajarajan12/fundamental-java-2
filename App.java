ae32e31a8678dbf1308470896f1bc915cf49bce5
gajarajan12 <110888287+gajarajan12@users.noreply.github.com>
2022-08-15 08:15:31 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+package fundamentalbasic.corejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("name : gajarajan,iniya,ramasamy,jaya");
+        System.out.println("place : chinna seeragapadi");
+        System.out.println("district : salem ");
+       System.out.println("fridge");
+        
+    }
+}
---
+++ Sample.java
@@ -0,0 +1,8 @@
+package fundamentalbasic.corejava;
+
+public class Sample {
+public static void main(String[] args) {
+	System.out.println("HELLO JAVA.sugfiwrw..");
+	System.out.println("gaja kanna manoj bro");
+}
+}
